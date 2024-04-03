6bbf852d60d611df7c166775f5bbd355209e9f3f
子炫 沐 <973016549@qq.com>
2024-04-03 13:11:13 +0800

    1

---
+++ virtual_hospital_system/src/Layout/AdminLayout.vue
@@ -0,0 +1,79 @@
+<template>
+
+    <div class="common-layout">
+    
+        <el-container>
+    
+          <el-header>虚拟宠物医院学习系统</el-header>
+    
+          <el-container>
+    
+            <el-aside width="200px">
+           <div>
+        
+          <el-menu :default-active="activeIndex" mode="vertical" @select="handleMenuSelect">
+            <el-scrollbar>
+            <el-menu-item index="/3dviews" @click="navigateTo('/3dviews')">
+              3 D 导览
+            </el-menu-item>
+            <el-menu-item index="/functionalLearning" @click="navigateTo('/functionalLearning')">
+              职能学习
+            </el-menu-item>
+            <el-menu-item index="/testUser" @click="navigateTo('/testUser')">
+              测试模块
+            </el-menu-item>
+            <el-menu-item index="/mineInformation" @click="navigateTo('/mineInformation')">
+              个人信息
+            </el-menu-item>
+            <!-- 更多导航栏项 -->
+          </el-scrollbar>
+          </el-menu>
+        
+        </div>
+            </el-aside> 
+            <el-main>
+              <el-scrollbar ref="scrollbarRef" height="500px" always @scroll="scroll">
+    
+              <div>
+              <router-view></router-view>
+              </div>
+            </el-scrollbar>
+            </el-main>
+          </el-container>
+        </el-container>
+      </div>
+    
+      </template>
+      
+      <script>
+      import { useRouter } from 'vue-router'
+      export default {
+        setup(){
+        const router = useRouter();
+        const navigateTo = (path) => {
+          router.push(path);
+        };
+        return {
+          navigateTo
+        };
+      }
+      }
+      </script>
+    
+    <style scoped lang="scss">
+    .el-container{
+        .el-aside{
+    
+        width:230px;
+        }
+        .el-header{
+        height:80px;
+        position: relative;
+    
+        }
+    
+       
+    
+    }
+    
+    </style>

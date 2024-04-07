77e6a9ef4097e59905cc4c6ed83b81fd0fc684eb
Atlas1y <1245477387@qq.com>
2024-04-07 09:54:06 +0800

    new

--- virtual_hospital_system/src/api/api.js
+++ virtual_hospital_system/src/api/api.js
@@ -10,4 +10,115 @@ export const login = data => {
   })
 }
 
+export const getAllCases = data => {
+  return get ({
+    url: '/cases/get_all_cases',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data
+  })
+}
 
+export const changeCname = data => {
+  return get ({
+    url: '/cases/change_cname',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data
+  })
+}
+
+export const changePhoto1 = data => {
+  return get ({
+    url: '/cases/change_photo1',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data
+  })
+}
+
+export const changePhoto2 = data => {
+  return get ({
+    url: '/cases/change_photo2',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data
+  })
+}
+
+export const changeType = data => {
+  return get ({
+    url: '/cases/change_type',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data
+  })
+}
+export const changeVideo4 = data => {
+  return get ({
+    url: '/cases/change_video4',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data
+  })
+}
+export const changeWord1 = data => {
+  return get ({
+    url: '/cases/change_word1',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data
+  })
+}
+export const changeWord2 = data => {
+  return get ({
+    url: '/cases/change_word2',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data
+  })
+}
+export const changeWord3 = data => {
+  return get ({
+    url: '/cases/change_word3',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data
+  })
+}
+export const changeWord4 = data => {
+  return get ({
+    url: '/cases/change_word4',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data
+  })
+}
+export const deleteCase = data => {
+  return get ({
+    url: '/cases/delete_case',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data
+  })
+}
+export const insertCase = data => {
+  return get ({
+    url: '/cases/insert_case',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data
+  })
+}
--- virtual_hospital_system/src/components_admin/caseManage.vue
+++ virtual_hospital_system/src/components_admin/caseManage.vue
@@ -1,5 +1,75 @@
 <template>
-    <el-button type="primary">新增病例</el-button>
+    <div style="padding: 20px;">
+
+        <el-button type="primary" style="margin-bottom: 20px;" @click="addCase">新增病例</el-button>
+
+        <el-card style="margin-bottom: 20px;" v-for="thisCase in caseList" :key="thisCase.cid">
+            <div>
+                <span>{{ thisCase.cname }}</span>
+                <el-button type="text" @click="editCase(thisCase)">编辑</el-button>
+                <el-button type="text" @click="deleteCase(thisCase.cid)">删除</el-button>
+            </div>
+            <div>
+                <p style="color: darkgray;">文字记录1：</p>
+                <p>{{ thisCase.word1 }}</p>
+            </div>
+            <div>
+                <p style="color: darkgray;">文字记录2：</p>
+                <p>{{ thisCase.word2 }}</p>
+            </div>
+            <div>
+                <p style="color: darkgray;">文字记录3：</p>
+                <p>{{ thisCase.word3 }}</p>
+            </div>
+            <div>
+                <p style="color: darkgray;">文字记录4：</p>
+                <p>{{ thisCase.word4 }}</p>
+            </div>
+            <div >
+                <img :src="thisCase.photo1" class="caseImg"></img>
+                <img :src="thisCase.photo2" class="caseImg"></img>
+            </div>
+            <div>
+                <video-player src="thisCase.video4"></video-player>
+            </div>
+        </el-card>
+    </div>
+    
 </template>
-<script></script>
-<style></style>
+<script setup>
+import { reactive,ref,onMounted  } from 'vue';
+import { VideoPlayer } from '@videojs-player/vue'
+import 'video.js/dist/video-js.css'
+import { getAllCases,changeCname,changePhoto1,changePhoto2,changeType,changeWord1,changeWord2,changeWord3,changeWord4 } from '@/api/api';
+
+
+
+const caseList = ref([])//定义响应式数组变量，存放全部病例
+
+onMounted(()=>{
+    getAllCases().then(res=>{
+        console.log(res)
+        caseList.value=res.data
+    })
+})
+
+const editCase = (thisCase) => {
+
+};
+
+const deleteCase = (cid) => {
+
+}
+
+const addCase = () => {
+
+}
+</script>
+<style>
+.caseImg
+{
+    width:auto;
+    height:200px;
+    margin-right: 20px;
+}
+</style>

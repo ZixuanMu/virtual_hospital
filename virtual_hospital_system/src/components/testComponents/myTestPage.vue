6caf83f3ad9380394ddbf9d404e491f96985be5b
子炫 沐 <973016549@qq.com>
2024-04-12 08:45:24 +0800

    1

--- virtual_hospital_system/src/components/testComponents/myTestPage.vue
+++ virtual_hospital_system/src/components/testComponents/myTestPage.vue
@@ -49,7 +49,8 @@ export default {
         question.contentA,
         question.contentB,
         question.contentC,
-        question.contentD
+        question.contentD,
+
       ];
     },
     getResult(questionIndex) {
